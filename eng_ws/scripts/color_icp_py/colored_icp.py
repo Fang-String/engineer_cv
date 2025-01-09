e81cf39befb5de597dc35074dc68dc0d2c1e7d07
FangString <star_str1ng@163.com>
2025-01-09 16:37:51 +0800

    好用的pyicp

--- eng_ws/scripts/color_icp_py/colored_icp.py
+++ eng_ws/scripts/color_icp_py/colored_icp.py
@@ -25,16 +25,18 @@ def icp_registration(source, target, max_corres_dist):
     draw_registration_result(source, target, result.transformation, color=False)
 
 
-def colored_icp_registration(source, target, voxel_size):
+def colored_icp_registration(source, target, voxel_size,init_trans=np.identity(4)):
     print("Colored ICP registration")
     voxel_radius = [5*voxel_size, 3*voxel_size, voxel_size]
-    max_iter = [60, 35, 20]
+    max_iter = [70, 45, 30]
     current_transformation = np.identity(4)
+    source_cp = copy.deepcopy(source)
+    source_cp.transform(init_trans)
     for scale in range(3):
         max_it = max_iter[scale]
         radius = voxel_radius[scale]
         print("scale_level = {0}, voxel_size = {1}, max_iter = {2}".format(scale, radius, max_it))
-        source_down = source.voxel_down_sample(radius)
+        source_down = source_cp.voxel_down_sample(radius)
         target_down = target.voxel_down_sample(radius)
         source_down.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius*2, max_nn=20))
         target_down.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius*2, max_nn=20))
@@ -49,7 +51,9 @@ def colored_icp_registration(source, target, voxel_size):
                                                               max_iteration=max_it))
         current_transformation = result.transformation
         print(result)
+    current_transformation = current_transformation@init_trans
     print(current_transformation)
+    return current_transformation
     # draw_registration_result(source, target, current_transformation, color=True)
 
 
@@ -62,25 +66,24 @@ if __name__ == "__main__":
     # parameters
     voxel_size = 0.01
     max_corres_dist = 5*voxel_size
-    trans_init = np.asarray([
-        [-0.51      , -0.86 , 0.       ,  0.09      ],
-        [ 0.86 ,-0.49      ,  0.       ,  0.22      ],
-        [ 0.     ,    0.    ,     1.      ,   0.145     ],
-        [ 0.     ,    0.      ,   0.     ,    1.       ]])
+    trans_init = np.asarray([[ 0.35355339 ,-0.5732233   ,0.73919892 , 5.2   ],
+                            [ 0.61237244 , 0.73919892 , 0.28033009 , -2.9  ],
+                            [-0.70710678,  0.35355339,  0.61237244, 10.07    ],
+                            [ 0.  ,        0. ,         0. ,         1.   ]])
     transformation = trans_init
     # trans_init = np.asarray([[0.862, 0.011, -0.507, 0.5],
     #                         [-0.139, 0.967, -0.215, 0.7],
     #                         [0.487, 0.255, 0.835, -1.4], [0.0, 0.0, 0.0, 1.0]])
 
     # load point clouds
-    target = o3d.io.read_point_cloud("/home/ubuntu/step2pcd/transformed_cube.pcd")
-    source = o3d.io.read_point_cloud("/home/ubuntu/step2pcd/cube.pcd")
+    target = o3d.io.read_point_cloud("/home/ubuntu/step2pcd/t2.pcd")
+    source = o3d.io.read_point_cloud("/home/ubuntu/step2pcd/c2.pcd")
     # source = o3d.io.read_point_cloud("../data/frag_115.ply")
     # target = o3d.io.read_point_cloud("../data/frag_116.ply")
     print("Loaded " + str(len(source.points)) + " points for source point cloud")
     print("Loaded " + str(len(target.points)) + " points for target point cloud")
 
-    source.transform(trans_init)
+    # source.transform(trans_init)
     # estimate normals
     # source.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=2*voxel_size, max_nn=16))
     # target.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=2*voxel_size, max_nn=16))
@@ -97,7 +100,7 @@ if __name__ == "__main__":
 
     # colored ICP
 
-    colored_icp_registration(source, target, voxel_size)
-    # draw_registration_result(source, target, transformation, color=True)
+    current_transformation=colored_icp_registration(source, target, voxel_size,trans_init)
+    # draw_registration_result(source, target, current_transformation, color=True)
     end_time = time.time()  # 记录结束时间
     print(f"Colored ICP registration took {end_time - start_time:.4f} seconds")  # 计算并打印耗时
